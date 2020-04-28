320cef91854f89679fecd543f60877db911c60dc
unknown <xodud7965@naver.com>
2020-04-28 14:01:59 +0900

    project-ex07

---
+++ project-ex07/src/FoodListItem.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+export default class FoodListItem extends React.Component{
+    
+    render(){
+        console.log(this.props);
+        return (            
+                <li>{this.props.name} : {this.props.quantity}</li>
+             
+        );
+    }
+}
