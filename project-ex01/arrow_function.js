2e6950c841a8a85e1651ab76fb1cfdf183c6ff76
unknown <xodud7965@naver.com>
2020-04-29 12:28:54 +0900

    kanbanBoard#2

---
+++ project-ex01/arrow_function.js
@@ -0,0 +1,7 @@
+const f = function(x){
+    return x * x;
+}
+
+a => a*a;
+
+console.log(f(10));
--- project-ex07/src/App.js
+++ project-ex07/src/App.js
@@ -1,10 +1,11 @@
+import FoodList from './FoodList';
 import React from 'react';
 
 class App extends React.Component {
     render() {
         const message = 'Hello React';
         return (
-            <h1>{ message }</h1>
+            <FoodList />
         );
     }
 }
